99868f64e9e01c868bfcca79a2a32f573ee1d841
Noah Sohn <gonoah510@gmail.com>
2023-04-12 13:20:06 -0700

    fixed the stats embed and relocated file

--- commands/utilities/statistics.js
+++ commands/utilities/statistics.js
@@ -1,18 +1,18 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('statistics')
 		.setDescription("Checks the bot's uptime and latency"),
 	async execute(interaction) {
-		const sent = await interaction.reply({ content: 'Checking...', fetchReply: true });
+		const sent = await interaction.reply({ content: 'Checking...', fetchReply: true, ephemeral: true });
 		const statisticsEmbed = new EmbedBuilder()
 			.setTitle('Bot Statistics')
 			.addFields(
-				{ name: 'Uptime', value: `${Math.round(interaction.client.uptime / 60000)} minutes` },
+				{ name: 'Uptime', value: `${Math.round(interaction.client.uptime / 60000)} minutes`, inline: true },
 				{ name: 'Websocket Heartbeat', value: `${interaction.client.ws.ping}ms`, inline: true },
 				{ name: 'Rountrip Latency', value: `${sent.createdTimestamp - interaction.createdTimestamp}ms`, inline: true },
-			)
-		await interaction.editReply( embeds: [statisticsEmbed] );
+			);
+		await interaction.editReply({ content: '', embeds: [statisticsEmbed], ephemeral: true });
 	},
 };
