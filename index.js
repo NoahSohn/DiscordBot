d7f2b15da2e4c5d98cb7bc52b31d82d954541c42
Noah Sohn <gonoah510@gmail.com>
2023-04-12 11:09:31 -0700

    login to discord

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+//Require the dotenv library to use the token from .env
 const dotenv = require('dotenv');
 
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+//Configure dotenv
 dotenv.config();
 
-console.log(process.env.DISCORD_BOT_TOKEN);
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(process.env.DISCORD_BOT_TOKEN);
