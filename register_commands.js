2c00cb3a0690ad9a2d4d99a739bfb3ecbb1fc2d0
Noah Sohn <gonoah510@gmail.com>
2023-04-12 13:32:52 -0700

    fixed reading commands from folders inside folders also changes the name of the commands directory to be in config.json

--- register_commands.js
+++ register_commands.js
@@ -9,39 +9,54 @@ const fs = require('node:fs');
 const path = require('node:path');
 
 //Get configuration settings from config.js
-const { clientId, guildId } = require('./config.json');
+const { clientId, guildId, commandsFolder } = require('./config.json');
 
 //Configure dotenv
 dotenv.config();
 
-//Read commands from the "commands" folder
+console.log('commandsFolder:', commandsFolder);
+
+// Read commands from the "commands" folder and its subdirectories
 const commands = [];
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandsPath = path.join(__dirname, commandsFolder);
+const commandFiles = getCommandFiles(commandsPath);
+
+// Helper function to get all .js files in a directory and its subdirectories
+function getCommandFiles(dir) {
+  const dirents = fs.readdirSync(dir, { withFileTypes: true });
+  const files = dirents
+    .filter(dirent => !dirent.isDirectory() && dirent.name.endsWith('.js'))
+    .map(dirent => path.join(dir, dirent.name));
+  const folders = dirents.filter(dirent => dirent.isDirectory());
+  for (const folder of folders) {
+    files.push(...getCommandFiles(path.join(dir, folder.name)));
+  }
+  return files;
+}
 
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
+  const command = require(file);
+  commands.push(command.data.toJSON());
 }
 
 // Construct and prepare an instance of the REST module
-const rest = new REST().setToken(process.env.DISCORD_BOT_TOKEN);
+const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_BOT_TOKEN);
 
-//Deploy  commands
+// Deploy commands
 (async () => {
-	try {
-		console.log(`Started updating ${commands.length} application commands.`);
+  try {
+    console.log(`Started updating ${commands.length} application commands.`);
 
-		// The put method is used to fully refresh all commands in the guild with the current set
-		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
-			{ body: commands },
-		);
+    // The put method is used to fully refresh all commands in the guild with the current set
+    const data = await rest.put(
+      Routes.applicationGuildCommands(clientId, guildId),
+      { body: commands },
+    );
 
-		console.log(`Successfully updated ${data.length} application commands.`);
-	} catch (error) {
-		// Catch and log any errors!
-		console.error(error);
-	}
+    console.log(`Successfully updated ${data.length} application commands.`);
+  } catch (error) {
+    // Catch and log any errors!
+    console.error(error);
+  }
 })();
